988230824d74d49bccf501821b18bdb4d04b1797
hannahtreadway <treadwayhannah@ufl.edu>
2025-03-23 01:32:24 +0000

    Deploying to gh-pages from @ hannahtreadway/hannahtreadway.github.io@cd2748cbcfb5bd9e49104baf54d592bf7fadbcd1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -490,35 +490,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%74%72%65%61%64%77%61%79%68%61%6E%6E%61%68@%75%66%6C.%65%64%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-orcid',
+        title: 'ORCID',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://orcid.org/https://orcid.org/0000-0001-6244-7674", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-osf',
+        title: 'Open Science Framework',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://osf.io/osf.io/8fydm/", "_blank");
         },
       },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://bsky.app/profile/treadwayhannah.bsky.social", "_blank");
         },
       },{
       id: 'light-theme',
