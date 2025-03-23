047ca0506e3df6fd7fdf3effa92d59b68b371ebc
hannahtreadway <treadwayhannah@ufl.edu>
2025-03-23 01:47:46 +0000

    Deploying to gh-pages from @ hannahtreadway/hannahtreadway.github.io@b11fa2b1ff2834ad6a500ea63596ae345937ba27 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -402,8 +402,8 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-join-me-at-hsp2025-at-umd-for-my-presentation-on-recent-work-entitled-cognitive-control-adaptation-and-code-switching-an-erp-study",
+          title: 'Join me at HSP2025 at UMD for my presentation on recent work entitled...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
