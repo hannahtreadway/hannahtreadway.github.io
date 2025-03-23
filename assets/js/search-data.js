b338da3f52954b213d9fa118ad5a36905594098c
hannahtreadway <treadwayhannah@ufl.edu>
2025-03-23 03:38:38 +0000

    Deploying to gh-pages from @ hannahtreadway/hannahtreadway.github.io@c76fcd52505becf64d51c71be3f200cd273de9f2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -424,7 +424,7 @@ ninja.data = [{
               window.location.href = "/projects/4_project/";
             },},{id: "projects-effects-of-perceived-accent-on-code-switching-processing",
           title: 'Effects of Perceived Accent on Code-Switching Processing',
-          description: "a project with a background image",
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-new-language-training-promotes-neurocognitive-resilience-in-aging",
