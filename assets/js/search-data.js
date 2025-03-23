cb83cff64e949bc68383a20bb5a41dfa74bfa2a1
hannahtreadway <treadwayhannah@ufl.edu>
2025-03-23 02:01:58 +0000

    Deploying to gh-pages from @ hannahtreadway/hannahtreadway.github.io@0650abcf4ac557ed7cfe9d02ce8838583aea2d9b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -417,8 +417,8 @@ ninja.data = [{
           description: "another without an image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
+            },},{id: "projects-effects-of-perceived-accent-on-code-switching-processing",
+          title: 'Effects of Perceived Accent on Code-Switching Processing',
           description: "a project with a background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
