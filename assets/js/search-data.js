5b66efd31e0834f54d2061b85a9032ce331fe40b
hannahtreadway <treadwayhannah@ufl.edu>
2026-05-14 20:12:25 +0000

    Deploying to gh-pages from @ hannahtreadway/hannahtreadway.github.io@146358394e1225a5f08bb7c2f14fc1194cccaee1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -14,7 +14,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/assets/pdf/TreadwayCV_May2026.pdf";
+            window.location.href = "/assets/pdf/TreadwayCV_24March2026.pdf";
           },
         },{id: "nav-projects",
           title: "projects",
